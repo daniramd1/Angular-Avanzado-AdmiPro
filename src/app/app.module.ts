deb7ee2ccf0e735765d34585a514dd75fec777e1
Daniel Radilla <danielramd@hotmail.com>
2022-03-28 15:53:30 -0600

    pagina seccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+//Rutas
+
+//componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadcrumsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    DashboardComponent,
+    Grafica1Component,
+    ProgressComponent,
+    NopagefoundComponent,
+    PagesComponent,
+    LoginComponent,
+    RegisterComponent
+ 
   ],
   imports: [
     BrowserModule,
