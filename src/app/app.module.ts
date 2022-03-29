bc8360c63d5565a4a44e578b2adc95c68529803b
Daniel Radilla <danielramd@hotmail.com>
2022-03-28 18:06:44 -0600

    Fin seccion 4 - Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,43 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 
-//Rutas
+//Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+
 
 
 //componentes
 
 import { AppComponent } from './app.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BreadcrumsComponent,
-    HeaderComponent,
-    SidebarComponent,
-    DashboardComponent,
-    Grafica1Component,
-    ProgressComponent,
     NopagefoundComponent,
-    PagesComponent,
-    LoginComponent,
-    RegisterComponent
- 
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
